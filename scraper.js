be86ad5feb8b72e27057de6136d6985f69bd2c92
rorvts <rorvts@gmail.com>
2020-07-06 18:09:14 +0200

    Get names and links to the data pages

--- scraper.js
+++ scraper.js
@@ -1,11 +1,20 @@
 const rp = require("request-promise");
 const $ = require("cheerio");
-const url = "https://www.ncbi.nlm.nih.gov/geo/browse/?view=samples&display=500&series=15745&sort=type";
+const url = `https://www.ncbi.nlm.nih.gov/geo/browse/?view=samples&series=15745&sort=type&display=500&page=1`;
+const filter = "genomic";
+const serie = [];
 
 rp(url)
-  .then(function(html){
-      console.log($("tbody > tr > td:not([class]):first-child > a", html).text().split(/(\S{9})/gm));
+  .then(function (html) {
+    for (tr = 2; tr <= 3; tr++) {
+      if ($(`tbody > tr:nth-child(${tr}) > td:not([class]):nth-child(3) > a`, html).text().substring(0, 7) === filter) {
+        serie.push([
+          $("tbody > tr:nth-child(2) > td:not([class]):first-child > a", html).text().split(/(\S{9})/gm).join(""),
+          $("tbody > tr:nth-child(2) > td:not([class]):first-child > a", html).attr("href")]);
+      }
+    }
+    console.log(serie)
   })
-  .catch(function(error){
+  .catch(function (error) {
     console.log(error);
   });
