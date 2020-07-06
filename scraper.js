b1fbb6817db79ae575d942e40d0d1f8923aae11a
rorvts <rorvts@gmail.com>
2020-07-06 19:24:52 +0200

    Get the next url from a selected sample

--- scraper.js
+++ scraper.js
@@ -6,7 +6,7 @@ const serie = [];
 
 rp(url)
   .then(function (html) {
-    for (tr = 2; tr <= 3; tr++) {
+    for (tr = 2; tr <= 2; tr++) {
       if ($(`tbody > tr:nth-child(${tr}) > td:not([class]):nth-child(3) > a`, html).text().substring(0, 7) === filter) {
         serie.push([
           $("tbody > tr:nth-child(2) > td:not([class]):first-child > a", html).text().split(/(\S{9})/gm).join(""),
@@ -14,7 +14,15 @@ rp(url)
       }
     }
     console.log(serie)
+    var newUrl = `https://www.ncbi.nlm.nih.gov${serie[0][1]}`
+    console.log(newUrl)
+    rp(newUrl)
+      .then(function (html2) {
+        console.log($("input[name='fulltable']", html2).attr('onclick'));
+      })
+      .catch()
   })
+  
   .catch(function (error) {
     console.log(error);
   });
