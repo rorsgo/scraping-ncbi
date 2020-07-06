5f62a435bbd7802ecae3dc23e89378f7f0b85936
rorvts <rorvts@gmail.com>
2020-07-06 23:19:33 +0200

    Fill the array with genomic sample names

--- scraper.js
+++ scraper.js
@@ -1,28 +1,32 @@
 const rp = require("request-promise");
 const $ = require("cheerio");
-const url = `https://www.ncbi.nlm.nih.gov/geo/browse/?view=samples&series=15745&sort=type&display=500&page=1`;
+const { nextTick } = require("process");
+const url = `https://www.ncbi.nlm.nih.gov/geo/browse/?view=samples&series=15745&display=500&zsort=type&page=1`;
 const filter = "genomic";
 const serie = [];
 
 rp(url)
   .then(function (html) {
-    for (tr = 2; tr <= 2; tr++) {
-      if ($(`tbody > tr:nth-child(${tr}) > td:not([class]):nth-child(3) > a`, html).text().substring(0, 7) === filter) {
-        serie.push([
-          $("tbody > tr:nth-child(2) > td:not([class]):first-child > a", html).text().split(/(\S{9})/gm).join(""),
-          $("tbody > tr:nth-child(2) > td:not([class]):first-child > a", html).attr("href")]);
-      }
+    const totalPage = $("span[id='page_cnt']", html).text();
+    for (page = 1; page <= totalPage; page++) {
+      var newUrl = `https://www.ncbi.nlm.nih.gov/geo/browse/?view=samples&series=15745&display=500&sort=type&page=${page}`
+      rp(newUrl)
+        .then(function (html) {
+          const totalTableRaw = ($("tr", html).length) - 2
+          for (tableRaw = 2; tableRaw <= totalTableRaw; tableRaw++) {
+            if ($(`tbody > tr:nth-child(${tableRaw}) > td:nth-child(3) > a`, html).text().substring(0, 7) === filter) {
+              serie.push($(`tbody > tr:nth-child(${tableRaw}) > td:not([class]):first-child > a`, html).text().split(/(\S{9})/gm).join(""));
+              console.log(serie.length)
+            } else {
+              return
+            }
+          }
+          console.log(serie)
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
     }
-    console.log(serie)
-    var newUrl = `https://www.ncbi.nlm.nih.gov${serie[0][1]}`
-    console.log(newUrl)
-    rp(newUrl)
-      .then(function (html2) {
-        console.log($("input[name='fulltable']", html2).attr('onclick'));
-      })
-      .catch()
   })
-  
-  .catch(function (error) {
-    console.log(error);
-  });
+  .catch()
+
