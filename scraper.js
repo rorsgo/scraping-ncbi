742f518e7e97967daed228d8ad83b97d9ab5cc5a
rorvts <rorvts@gmail.com>
2020-07-06 16:31:47 +0200

    Get an specific cell in the HTML page

--- scraper.js
+++ scraper.js
@@ -1,9 +1,10 @@
 const rp = require("request-promise");
-const url = "https://www.ncbi.nlm.nih.gov/geo/browse/?view=samples&series=15745&display=20&sort=type";
+const $ = require("cheerio");
+const url = "https://www.ncbi.nlm.nih.gov/geo/browse/?view=samples&display=500&series=15745&sort=type";
 
 rp(url)
   .then(function(html){
-    console.log(html);
+      console.log($("tbody > tr > td:not([class]):first-child > a", html).text().split(/(\S{9})/gm));
   })
   .catch(function(error){
     console.log(error);
